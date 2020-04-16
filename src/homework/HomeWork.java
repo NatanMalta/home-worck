d55e5402e5c96ed90137980b9b071a91da21bcc9
NatanMalta <natanmb13@gmail.com>
2020-04-16 19:00:23 -0300

    Teste com Jpanel

--- src/homework/HomeWork.java
+++ src/homework/HomeWork.java
@@ -1,7 +1,12 @@
 package homework;
 
 import apresentacao.frmPrincipal;
+import apresentacao.frmTeste;
 import controle.Numeros;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import apresentacao.JPanelTeste;
 
 
 public class HomeWork
@@ -12,7 +17,26 @@ public class HomeWork
     public  static void main(String[] args)
     {
         
-        frmPrincipal frmP = new frmPrincipal();
+        /*
+         frmTeste myFrame = new frmTeste();
+        
+        //myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Se possível, gostaria tambem de saber o que essa linha faz? Não vi diferença sem ela.
+        //myFrame.setSize(400,300); 
+        JPanelTeste jp = new JPanelTeste();
+        
+
+
+        myFrame.getContentPane().add(BorderLayout.CENTER,jp);
+        myFrame.setVisible(true);
+        */
+        
+        
+        frmTeste frmP = new frmTeste();
+        
+        
+        
+   
+        
         frmP.setVisible(true);
         
     }
